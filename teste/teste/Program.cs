6673e2377fd83cba255f60582c2f07bd0c938a62
GuiKoboldt <guikoboldt@hotmail.com>
2016-04-15 17:30:52 -0300

    Added -> dataBase to the solution Added -> using statement returning text file Added ->  yield and IEnumerable structure

--- teste/teste/Program.cs
+++ teste/teste/Program.cs
@@ -8,45 +8,35 @@ namespace teste
 {
     class Program
     {
+        static public IEnumerable<string> getLines(string path)
+        {
+            using (var file = new System.IO.StreamReader(path)) //read the file and put it into file variable
+            {
+                var line = default(string); //create a general variable, which run as string bt default
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    yield return line; //keep the method running and return item by item, in this case line by line
+                } //this structure just works with IEnumerable, because of the iterator
+            }
+        }
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("Test");
-            //int a = 1;
-            //Console.WriteLine(a);
-            //Console.ReadLine();
+            var path = "teste.txt";
 
-            //List<string> test = new List<string>(from a in "The quick brown fox jumps over the lazy dog".Split() orderby a.Length select a);
-            //from variable in table orderby select
-            System.IO.StreamReader file = new System.IO.StreamReader("teste.txt"); //read the file and put it into file variable
-            int countLine = 0;
-            string line;
-            string[] statements = new string[1];
-
-            while ((line = file.ReadLine()) != null)
-            { 
-                countLine++;
-            }
-            file.Close();
-
-            
-            statements = new string[countLine];
-
-            file = new System.IO.StreamReader("teste.txt");
-            countLine = 0;
-
-            while ((line = file.ReadLine()) != null)
+            foreach (var line in getLines(path))
             {
-                statements[countLine] = line;
-                countLine++;
+                Console.WriteLine(line);
             }
 
-            //string b = "Hey you!";
-            List<string> test = new List<string>(from a in statements where statements[0] == a select a); //from variable location where select variable
-            foreach (var text in test)
-            {
-                Console.WriteLine(text);
-            }
-
+            //var statements = GetLines(path).ToArray();
+            ////string b = "Hey you!";
+            //List<string> test = new List<string>(from a in statements  select a); //from - variable - location - where - select
+            //foreach (var text in test)
+            //{
+            //    Console.WriteLine(text);
+            //}
 
             Console.WriteLine("Update from home");
 
