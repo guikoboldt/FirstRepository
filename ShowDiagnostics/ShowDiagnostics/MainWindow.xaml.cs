8cf47fae6d23745f4269a490e15f5783e6610314
GuiKoboldt <guikoboldt@hotmail.com>
2016-04-20 17:57:17 -0300

    Added -> Tag TotalRam and AvailableRam

--- ShowDiagnostics/ShowDiagnostics/MainWindow.xaml.cs
+++ ShowDiagnostics/ShowDiagnostics/MainWindow.xaml.cs
@@ -25,14 +25,20 @@ namespace ShowDiagnostics
         {
             InitializeComponent();
             PerformanceCounter cpuInfo = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-            PerformanceCounter ramInfo = new PerformanceCounter("Memory", "Available MBytes");
+            PerformanceCounter ramInfo = new PerformanceCounter("Memory", "Available MBytes"); //get available memory
+
+            var ramInfoAvailable = Decimal.Round(Convert.ToDecimal(ramInfo.NextValue() / 1024), 2);
+
+            //ramInfo = new PerformanceCounter("Memory", "Total Physical Memory");
+            
+            var ramInfoTotal = new Compt
 
             //cpuInfo.CategoryName = "Processor";
             //cpuInfo.CounterName = "% Processor Time";
             //cpuInfo.InstanceName = "_Total";
 
             cpuInformation.Text = cpuInfo.NextValue().ToString();
-            ramInformation.Text = ramInfo.NextValue().ToString();
+            ramInformation.Text = (ramInfoTotal - ramInfoAvailable) + " GB".ToString(); //show the available memory in Gb
         }
     }
 }
