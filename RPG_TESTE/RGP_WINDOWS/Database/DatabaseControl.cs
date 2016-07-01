acb8b523ab01b49b819c9fa12b26d8ac6c5053dd
Guilherme Koboldt <guikoboldt@hotmail.com>
2016-07-01 17:37:44 -0300

    Added - new database to project created a new table called USER imput 2 rows in the table for tests

---
+++ RPG_TESTE/RGP_WINDOWS/Database/DatabaseControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGP_WINDOWS.Database
+{
+    class DatabaseControl
+    {
+        static void CheckUser (string nickName, string password)
+        {
+           
+        }
+    }
+}
