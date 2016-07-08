422356eb9251d42fc0ff22c492491d484b59add7
Guilherme Koboldt <guikoboldt@hotmail.com>
2016-07-08 17:42:04 -0300

    Added new project "TEST_OPP_3" SelfHost is done

---
+++ TEST_OPP_3_SELFHOST + WPF/TEST_OPP_3_SELFHOST + WPF/Server Features/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TEST_OPP_3_SELFHOST___WPF.Server_Features
+{
+   public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "ActionAPI",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.MapHttpAttributeRoutes();
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
---
+++ TEST_OPP_3_SELFHOST + WPF/TEST_OPP_3_SELFHOST + WPF/Server Features/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TEST_OPP_3_SELFHOST___WPF.Server_Features
+{
+   public class TestController : ApiController
+    {
+
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage
+            {
+                StatusCode = System.Net.HttpStatusCode.OK,
+                Content = new StringContent("Server is communicanting"),
+            };
+        }
+    }
+}
