ebde0ebfe17253abc4c3c8358438117ba2eb7589
Guilherme Koboldt <guikoboldt@hotmail.com>
2018-08-20 17:00:19 -0300

    [High Performance - .NET CORE] -Started Test project

---
+++ PerformanceTests/PerformanceTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerformanceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
